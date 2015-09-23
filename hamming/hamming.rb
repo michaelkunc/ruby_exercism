47466080a81551e3230fac4c96b4fa2de1ecd273
michaelkunc <mjkunc@gmail.com>
2015-09-23 10:46:07 -0500

    refactored hamming solution

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -3,11 +3,10 @@ class Hamming
 
   def self.compute(strand_a, strand_b)
     validate_input(strand_a, strand_b)
-    strand_a.split('').zip(strand_b.split('')).map {|a, b| a==b}.count(false)
+    strand_a.split('').zip(strand_b.split('')).map { |a, b| a == b }.count(false)
   end
 
   def self.validate_input(strand_a, strand_b)
-    raise ArgumentError, 'Please enter two equal length strands' if strand_a.length != strand_b.length
+    fail ArgumentError, 'Please enter two equal length strands' if strand_a.length != strand_b.length
   end
-
 end
