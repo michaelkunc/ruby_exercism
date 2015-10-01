f5a3e1b904c3f7deca7f67489b6e3900518faef7
michaelkunc <mjkunc@gmail.com>
2015-09-30 20:02:21 -0500

    refactor raindrops

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,18 +1,11 @@
-require 'prime'
-
 class Raindrops
-
-  CONVERSION = {3 => 'Pling', 5 => 'Plang', 7 => 'Plong'}
-
+  CONVERSION = { 3 => 'Pling', 5 => 'Plang', 7 => 'Plong' }
   def self.convert(number)
-    prime_factors = Prime.prime_division(number).flatten.uniq
-    if (prime_factors & CONVERSION.keys).empty?
-      number.to_s
-    else
-      response = ''
-      prime_factors.each {|element| response <<  CONVERSION[element] unless CONVERSION[element].nil?}
-      response
-    end
+    words = create_response(number)
+    words.length > 1 ? words : number.to_s
   end
 
+  def self.create_response(number)
+    CONVERSION.map { |k, v| v if number % k == 0 }.join
+  end
 end
