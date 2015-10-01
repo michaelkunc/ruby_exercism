32469a9fed24dee283d3c41e48c40c0e2f844ad8
michaelkunc <mjkunc@gmail.com>
2015-09-30 20:04:28 -0500

    refactor raindrops

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -2,7 +2,7 @@ class Raindrops
   CONVERSION = { 3 => 'Pling', 5 => 'Plang', 7 => 'Plong' }
   def self.convert(number)
     words = create_response(number)
-    words.length > 1 ? words : number.to_s
+    words.empty? ? number.to_s : words
   end
 
   def self.create_response(number)
