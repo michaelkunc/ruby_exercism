fcce8fca7304e5c453b20c2edb56a7d63706720d
michaelkunc <mjkunc@gmail.com>
2015-10-21 11:22:50 -0500

    refactor word-count

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,16 +1,10 @@
 class Phrase
   VERSION = 1
   def initialize(phrase)
-    @phrase = extract_words(phrase)
+    @phrase = phrase.downcase.scan(/\b[\w']+\b/)
   end
 
   def word_count
     @phrase.each_with_object(Hash.new(0)) { |word, counts| counts[word] += 1 }
-  end
-
-  private
-
-  def extract_words(phrase)
-    phrase.downcase.scan(/\b[0-9A-Za-z']+\b/)
   end
 end
